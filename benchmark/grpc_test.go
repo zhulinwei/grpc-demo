a1b5bd78babf151788710cbc95108bdb2d642d87
Level.Z <1171567018@qq.com>
2019-09-27 11:25:38 +0800

    更新项目

--- benchmark/grpc_test.go
+++ benchmark/grpc_test.go
@@ -2,19 +2,13 @@ package benchmark
 
 import (
 	"context"
-	"encoding/json"
-	pb "github.com/zhulinwei/grpc-demo/helloworld/gin/proto"
+	pb "github.com/zhulinwei/grpc-demo/helloworld/greeter/proto"
 	"google.golang.org/grpc"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"testing"
 )
 
 const (
 	rpcAddress = "localhost:8080"
-	ginAddress = "localhost:8081"
-	//rpcAddress = "34.217.41.67:8082"
 )
 
 func BenchmarkGRPCP(b *testing.B) {
@@ -37,59 +31,4 @@ func BenchmarkGRPCP(b *testing.B) {
 			b.Fatalf("grpc response is wrong: %v", response)
 		}
 	}
-}
-
-func BenchmarkHTTP(b *testing.B) {
-	uri := url.URL{
-		Scheme:   "http",
-		Host:     ginAddress,
-		Path:     "/http",
-		RawQuery: "name=tony",
-	}
-	var err error
-	var response *http.Response
-
-	for n := 0; n < b.N; n++ {
-		if response, err = http.Get(uri.String()); err != nil {
-			b.Fatalf("http request failed: %v", err)
-		}
-		if response == nil {
-			b.Fatalf("http response is wrong: %v", response)
-		}
-
-		var body []byte
-		if body, err = ioutil.ReadAll(response.Body); err != nil {
-			b.Fatalf("http body is wrong: %v", err)
-		}
-		if err = response.Body.Close(); err != nil {
-			b.Fatalf("http body close fail: %v", err)
-		}
-
-		type Body struct {
-			//Message string
-			replyValue1  string
-			replyValue2  string
-			replyValue3  string
-			replyValue4  string
-			replyValue5  string
-			replyValue6  string
-			replyValue7  string
-			replyValue8  string
-			replyValue9  string
-			replyValue10 string
-			replyValue11 string
-			replyValue12 string
-			replyValue13 string
-			replyValue14 string
-			replyValue15 string
-			replyValue16 string
-			replyValue17 string
-			replyValue18 string
-			replyValue19 string
-			replyValue20 string
-		}
-		var obj Body
-		err = json.Unmarshal(body, &obj)
-	}
-	//fmt.Println(string(body))
 }
--- benchmark/http_test.go
+++ benchmark/http_test.go
@@ -1,47 +1,46 @@
 package benchmark
-//
-//import (
-//	"encoding/json"
-//	"io/ioutil"
-//	"net/http"
-//	"net/url"
-//	"testing"
-//)
-//
-//const (
-//	ginAddress = "localhost:8081"
-//)
-//
-//func BenchmarkHTTP(b *testing.B) {
-//	uri := url.URL{
-//		Scheme:   "http",
-//		Host:     ginAddress,
-//		Path:     "/http",
-//		RawQuery: "name=tony",
-//	}
-//	var err error
-//	var response *http.Response
-//
-//	for n := 0; n < b.N; n++ {
-//		if response, err = http.Get(uri.String()); err != nil {
-//			b.Fatalf("http request failed: %v", err)
-//		}
-//		if response == nil {
-//			b.Fatalf("http response is wrong: %v", response)
-//		}
-//
-//		var body []byte
-//		if body, err = ioutil.ReadAll(response.Body); err != nil {
-//			b.Fatalf("http body is wrong: %v", err)
-//		}
-//		if err = response.Body.Close(); err != nil {
-//			b.Fatalf("http body close fail: %v", err)
-//		}
-//		type Body struct {
-//			Message string
-//		}
-//		var obj Body
-//		err = json.Unmarshal(body, &obj)
-//	}
-//	//fmt.Println(string(body))
-//}
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+const (
+	ginAddress = "localhost:8081"
+)
+
+func BenchmarkHTTP(b *testing.B) {
+	uri := url.URL{
+		Scheme:   "http",
+		Host:     ginAddress,
+		Path:     "/http",
+		RawQuery: "name=tony",
+	}
+	var err error
+	var response *http.Response
+
+	for n := 0; n < b.N; n++ {
+		if response, err = http.Get(uri.String()); err != nil {
+			b.Fatalf("http request failed: %v", err)
+		}
+		if response == nil {
+			b.Fatalf("http response is wrong: %v", response)
+		}
+
+		var body []byte
+		if body, err = ioutil.ReadAll(response.Body); err != nil {
+			b.Fatalf("http body is wrong: %v", err)
+		}
+		if err = response.Body.Close(); err != nil {
+			b.Fatalf("http body close fail: %v", err)
+		}
+		type Body struct {
+			Message string
+		}
+		var obj Body
+		err = json.Unmarshal(body, &obj)
+	}
+}
